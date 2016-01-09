2b202a34e28397b1f1b2c23455860c0bc4bafce9
tminussi <tminussi@gmail.com>
2016-01-09 19:15:47 -0200

    Removendo target

--- target/classes/sql/popular-base-dados.sql
+++
@@ -1,5 +0,0 @@
-insert into restaurante values (1, 'Bar do Alemão', 'Alemã', 'bardoalemao.jpg', 0);
-insert into restaurante values (2, 'Divino Fogão', 'Brasileira', 'divinofogao.jpg', 0);
-insert into restaurante values (3, 'Gendai', 'Oriental', 'gendai.jpg', 0);
-insert into restaurante values (4, 'Outback', 'Americana', 'outback.jpg', 0);
-insert into restaurante values (5, 'Taco Loco', 'Mexicana', 'tacoloco.jpg', 0);
